cfbfbc723c625d34b609113b85d39f5143cf33dc
Jayan Fazal Karim <jfk303@nyu.edu>
2014-02-25 23:57:22 -0500

    Builds controllers rails side for app.

---
+++ app/controllers/feeds_controller.rb
@@ -0,0 +1,23 @@
+class FeedsController < ApplicationController
+  def index
+    @feedds = Feed.includes(:entries)
+    
+    @feeds = @feeds.to_json(include: :entries).html_safe
+    
+    respond_to do |format|
+      format.html { render :index }
+      format.json { render json: @feeds }
+    end
+  end
+  
+  def create
+    feed = Feed.find_or_create_by_url(params[:feed][:url])
+    feed_json = feed.to_json(include: :entries).html_safe
+    
+    if feed
+      render json: feed_json
+    else
+      render json: { error: "invalid url" }, status: :unprocessable_entity
+    end
+  end
+end
