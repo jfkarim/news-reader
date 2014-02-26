7281fb63c35a3bbab93c9db504631dd464ab01bf
Jayan Fazal Karim <jfk303@nyu.edu>
2014-02-25 23:41:10 -0500

    First commit. modifies gemfile/

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+NewsReader::Application.config.secret_token = '5dc0fc549528ea17ee92dd6a8c45affc1051a59635504ee23502c8a2ff642193ab209520791d6c9a4ef62c9ebb90a769702171f28a5bc5302cb5939a6b0374f0'
